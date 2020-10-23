a877973ef4e67e227666e7a8947843920bd87041
Sonal Sannigrahi <sannigrahisonal@gmail.com>
2020-10-24 00:02:21 +0200

    Update eval.mli

--- eval.mli
+++ eval.mli
@@ -1,17 +1,12 @@
 open Map
 open Ast
 
-module String_type_Pairs: 
-  sig 
-    type t
-    val compare: string -> string -> int
-  end
+type value
 
-module Eval:
-  sig 
-    type value
-    type env
-    val eval_expr: expr -> env -> value
-  end
+type env
 
-module Env : Map.S with type key = string
+val create_env : env
+
+val eval_expr: expr -> env -> value
+
+val print_value: out_channel -> value -> unit 
