2afe74867eb9083044613cb7d267d1b60f368ba6
diego <diegoandre.gomez@gmail.com>
2020-10-26 17:05:16 +0100

    finished type but rec

--- type.ml
+++ type.ml
@@ -1,6 +1,7 @@
 open Ast;;
 open Map;;
 open Printf;;
+open Printer;;
 
 module String_type_Pairs = 
   struct 
@@ -11,22 +12,57 @@ module String_type_Pairs =
 module Env = Map.Make(String_type_Pairs);;
 
 type typ =
-  | Tint                      (* base type int *)
-  | Tbool                     (* base type bool *)
-  | Tprod of typ * typ        (* product of two types *)
-  | Tarrow of typ list * typ * env  (* function type *)
+  | Tint                  
+  | Tbool                   
+  | Tprod of typ * typ        
+  | Tarrow of typ list * typ * (env ref)
 and env = typ Env.t;;
 
-let typ_env = Env.empty;;
+let typ_env = ref (Env.empty);;
 
 exception TypeError of string ;;
 
+(*QUESTION: why do we need \n ? it messes up pair
+  How do we print functions?*)
+
+let rec equality_types (t1:typ) (t2:typ) = 
+  match t1,t2 with 
+  | Tint, Tint -> true
+  | Tbool,Tbool -> true
+  | Tprod(t11,t12),Tprod(t21,t22) -> (equality_types t11 t21) && (equality_types t12 t22)
+  | _ -> false
+
+let rec typ_to_ast (t:typ) = 
+  match t with
+  | Tint -> Ast.Tint
+  | Tbool -> Ast.Tbool
+  | Tprod(t1,t2) -> Ast.Tprod((typ_to_ast t1), (typ_to_ast t2))
+  | Tarrow(l,t,env) -> (let l_type = ref [] in 
+                        for i=0 to ((List.length l)-1) do
+                        l_type := !l_type @ [typ_to_ast (List.nth l i)]
+                        done;
+                        Tarrow(!l_type,(typ_to_ast t)))
+let rec ast_to_typ (t:Ast.typ) (env:env ref) = 
+  match t with
+  | Ast.Tint -> Tint
+  | Ast.Tbool -> Tbool
+  | Ast.Tprod(t1,t2) -> Tprod((ast_to_typ t1 env), (ast_to_typ t2 env))
+  | Ast.Tarrow(l,t) -> (let l_type = ref [] in 
+                        for i=0 to ((List.length l)-1) do
+                        l_type := !l_type @ [ast_to_typ (List.nth l i) env]
+                        done;
+                        Tarrow(!l_type,(ast_to_typ t env),env))
+
+
 let rec print_type (out: out_channel) (t: typ) = 
   match t with
-  | Tbool -> fprintf out "bool \n"
-  | Tint -> fprintf out "int \n" 
-  | Tprod (t1,t2) -> fprintf out "pair = %a * %a \n" print_type t1 print_type t2 
-  | _ -> fprintf out "Not a base type"
+  | Tbool -> fprintf out "bool "
+  | Tint -> fprintf out "int " 
+  | Tprod (t1,t2) -> fprintf out "(%a * %a)" print_type t1 print_type t2 
+  (* | Tarrow(l,output_type,env) -> (for i=0 to ((List.length l)-1) do
+                                  print_type out (List.nth l i)
+                                  done; print_type out output_type) *)
+  |  _ -> raise (TypeError "function not printable")
 
 let type_unary (u : uop) (e : typ) : typ = 
   match u, e with 
@@ -47,15 +83,48 @@ let type_binary (u: bop) (e1: typ) (e2: typ) : typ =
   | Bceq, Tint, Tint -> Tbool (* compare:  integers, equal *)
   | _ , _,_ -> raise (TypeError "Invalid Binop: Type mismatch");;
 
-let type_expr (e:expr) (env:env):typ =
-  let rec type_aux (e:expr) (env:env):typ =
+let type_expr (e:expr) (env:env ref):typ =
+  let rec type_aux (e:expr) (env:env ref):typ =
   match e with
   | Econst(c) -> (match c with
                 |Cint(c) -> Tint
                 |Cbool(b) -> Tbool)
-  | Ename (name) -> Env.find name env
+  | Ename (name) -> Env.find name !env
   | Eunary(uop,e1) ->  type_unary uop (type_aux e1 env)
   | Ebinary(bop,e1,e2) -> type_binary bop (type_aux e1 env) (type_aux e2 env)
-  | _ -> raise (TypeError "Not done")
+  | Eif(cond,e1,e2) -> (match (type_aux cond env) with
+                        | Tbool -> (match (type_aux e1 env),(type_aux e2 env) with 
+                                  | Tbool, Tbool -> Tbool
+                                  | Tint, Tint   -> Tint
+                                  | _ , _ -> raise (TypeError "the then and else block must have the same type"))
+                        | _ -> raise (TypeError "Invalid condition"))
+
+  | Epair(e1,e2)  -> Tprod((type_aux e1 env),(type_aux e2 env))
+
+  | Efun(l, expr) -> (let l_type = ref [] in 
+                      for i=0 to ((List.length l)-1) do
+                      (l_type := !l_type @ [snd(List.nth l i)];
+                      env:= (Env.add (fst(List.nth l i)) (ast_to_typ(snd(List.nth l i)) env) !env))
+                      done;
+                      (ast_to_typ (Tarrow(!l_type,typ_to_ast(type_aux expr env))) env))
+
+  | Elet(is_rec,v,e1,e2) -> (if is_rec 
+                      then raise(TypeError "rec bruh")
+                      else 
+                      let u = type_aux e1 env in
+                      let envu =  ref (Env.add v u !env) in
+                      (type_aux e2 envu))
+
+  | Eapply(name , l) ->
+            (match name with
+              | Ename(name) -> (let func = (Env.find name !env) in
+                             
+                              (match func with
+                              | Tarrow(args,output,env_f) -> ( if (List.length args <> List.length l) then raise (TypeError "Incorrect number of arguments") else
+                                                            for i=0 to ((List.length l)-1) do
+                                                            (assert (equality_types (List.nth args i) (type_aux (List.nth l i) env)))
+                                                            done;output)
+                              | _ -> raise (TypeError "Not a func")))
+                | _ -> raise (TypeError "Not a name") )
   in
   type_aux e env;;
--- type.mli
+++ type.mli
@@ -4,8 +4,8 @@ open Ast
 type env
 type typ 
 
-val typ_env : env
+val typ_env : env ref
 
-val type_expr: expr -> env -> typ
+val type_expr: expr -> env ref -> typ
 
 val print_type: out_channel -> typ -> unit
