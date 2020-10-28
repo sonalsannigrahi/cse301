a192cff3cc33461c29e3649fd328ccc5fe29baab
Sonal Sannigrahi <sannigrahisonal@gmail.com>
2020-10-28 10:25:10 +0100

    change example

--- examples/t-12.ml
+++ examples/t-12.ml
@@ -1,1 +1,13 @@
-let rec f x = f x
+let rec f = fun (x:int) ->
+  let rec g = fun (y:int) ->
+    if y <= x then
+      f (y)
+    else
+      f (-y)
+  in
+  if x <= 0 then
+    5
+  else
+    g (f (x-3))
+in
+f 3
